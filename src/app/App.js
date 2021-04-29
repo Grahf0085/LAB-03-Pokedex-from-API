f1000ee6e6e6f3ae38e9d9bb5f71ca45bfd80abf
Tucker Hoog <tuckerhoog@tutanota.com>
2021-04-28 22:45:11 -0400

    start lab, add header, footer, main, little CSS

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,23 @@
 import { Component } from 'react';
 import './App.css';
 import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
 
 class App extends Component {
 
   render() {
     return (
+
       <div className="App">
-  
-        My React App...
-        <img src="acl-logo.png" className="temp-images" alt="acl logo" />
-      
+
+        <Header />
+
+        <main>
+
+        </main>
+
+        <Footer />
       </div>
     );
   }
